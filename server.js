d9e05519841603d6186881bdaf882756eb0f06f8
Restyled.io <commits@restyled.io>
2022-01-15 11:18:25 +0000

    Restyled by prettier

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-let express = require('express')
+let express = require("express");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("build"));
 
-app.listen(
-    PORT,
-    function() { console.log(`Static server listening on port ${PORT}!`); });
+app.listen(PORT, function () {
+  console.log(`Static server listening on port ${PORT}!`);
+});
