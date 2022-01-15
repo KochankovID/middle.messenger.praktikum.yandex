f4bd6e025940b6d6fe3a6b258606dd8dd743378c
ilyakochankov <ilyakochankov@yandex.ru>
2022-01-15 13:45:03 +0300

    add express server

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('build'));
+
+app.listen(PORT, function () {
+    console.log(`Static server listening on port ${PORT}!`);
+});
